0d745b803d8c1095029577d672c77006e0135dd0
trongdj <tronghahu@gmail.com>
2022-03-09 21:09:15 +0700

    first commit

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Hello Thy',
+    introDesc: `Tui có vài điều muốn hỏi,ok không ?`,
+    btnIntro: 'OK',
+    title: 'Bà khỏe rồi đúng không ? Oreo dở lắm phải không ?',
+    desc: 'Phải trả lời đó nha :> ',
+    btnYes: 'Oreo dở lắmmmmm !!! khỏe hay không kệ tui ?',
+    btnNo: ' Oreo ngon lắm tui khỏe rồi ',
+    question:'tự nhiên quác to thế :((',
+    reasonPlaceholder: 'tui có thiểu năng không ?',
+    btnReply: 'Gởi ! 💌💌',
+    reply: ' ông không thiểu năng ông thông minh vcl😊😊',
+    mess: 'tui biết mà không vần khen 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho tui nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://www.facebook.com/phaitrong.nguyenkhong.9'
+}
